5e96dccfa57d6f5e1c2e32b9fdfac673e5a183fc
Fun Planet <dipamdiptam@gmail.com>
2020-10-27 00:02:05 +0530

    initial commit

---
+++ CleanerBotArduino.ino
@@ -0,0 +1,26 @@
+String mode = "manual";
+
+// Code in setup() will run only once at start
+void setup() {
+
+}
+
+// Code in loop() will run repeatedly
+void loop() {
+  if(mode == "auto") {
+    // MOVEMENT AND OBSTACLE DETECTION
+    // read input from ultrasonic sensor
+    // if distance < 50 turn right
+  }
+
+
+
+  // BLUETOOTH AND REMOTE CONTROLLING
+  // recieve messages from bluetooth
+  // if message = "auto" then mode = "auto"
+  // if message = "f" move forward
+  // if message = "l" move left
+  // if message = "r" move right
+  // if message = "b" move backward
+
+}
